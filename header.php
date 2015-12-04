6d1999e1c23db77a4d82893d90537b209874217f
Benjamin LANET <benjamin.lanet@besse-signature.com>
2015-12-04 10:08:39 +0100

    Update viewport meta

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
